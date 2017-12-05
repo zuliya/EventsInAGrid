c660d56b16f7108c313a52b35bcbb93d89b454d4
Zuzukina <Zuzukina@student-10-245-145-4.wlan.dur.ac.uk>
2017-12-05 18:30:08 +0000

    added comments

--- latest.py
+++ latest.py
@@ -10,29 +10,30 @@ import numpy as np
 
 
 
-#Grid - GLOBAL
+# Grid - GLOBAL
 grid = []
 
-#Object for event
+# Initialise class event
 class event:
     id = None
     tickets = None
     price = None
     manhattanDistance = None
 
-    #Create object with id,tickets,pirce
+    # Create a constructor
     def __init__(self, id, tickets, price):
+        # Create objects (id,tickets,price)
         self.id = id
         self.tickets = tickets
         self.price = price
 
-#Validation Function - Checking inputs are in good val.
+# Validation Function - Checking inputs are in good val.
 def validateInput(x,y):
-    #Check if it is int.
+    # Check if it is int.
     try:
         x = int(x)
         y = int(y)
-        #check if is in range -10 to 10
+        # check if is in range -10 to 10
         if x > 10 or x < -10 or y > 10 or y < -10:
             print("ERROR: Please input the correct coordinates in range (-10 to 10).")
             return False
@@ -43,20 +44,21 @@ def validateInput(x,y):
         print("ERROR: Please enter integer co-ordinates.")
         return False
 
-#Function to convert coordinates to grid
+# Function to convert coordinates to grid
 def fixInput(x,y):
     x += 10
     y += 10
     return x,y
 
+# Calculate distanc using Simple Manhatan Distance
 def manhattanDistance(start, end):
     startX, startY = start
     endX, endY = end
-
-    #A,B = (Ax-Bx)+(Ay-By) Manhatan distabce formula
+    # A,B = (Ax-Bx)+(Ay-By) Manhatan distabce formula
     return abs(endX - startX) + abs(endY - startY)
 
-#Generate grid and generate seed. First to run.
+
+# Generate grid and generate seed. First to run.
 def seed():
     for x in range(0, 21):
         yAxis = []
@@ -64,58 +66,50 @@ def seed():
             yAxis.append(None)
         grid.append(yAxis)
 
-    #TODO - Check if the event exists at the point.
-    #Generate Seed
+    # TODO - Check if the event exists at the point.
 
-    #Generating uniqueID and checking if it exists already.
+    # Generate Seed (In the grid insert 10 events (uniqueID, rand ticket, rand price))
     usedIDs = []
-
     for counter in range(10):
-        #Generating ID
+        # Generating 10 random unique ID's
         while True:
+            #make sure id is unique int between 20
             id = np.random.randint(0, 20)
-            #check if id is taken
             if (id not in usedIDs):
                 usedIDs.append(id)
                 break
-        #generate 10 events (id, ticket,price)
         grid[np.random.randint(0,20)][np.random.randint(0,20)] = event(id,np.random.randint(0,20),np.random.randint(0,100))
 
-#Function to find events
+# Function to find events
 def findEvents(inputX,inputY):
-
-    #Distance => Events
+    # Events stored as dictionary so can sort by keys ( distances )
     events = {}
 
     #Finding events and populating the events array
     for x in range(len(grid)):
         for y in range(len(grid[x])):
-            #go through grid and find all events and their distance
             if grid[y][x] != None:
                 grid[y][x].manhattanDistance = manhattanDistance((inputX,inputY),(x,y))
+                # Distance = key ; points to a specific event
                 events[manhattanDistance((inputX,inputY),(x,y))] = grid[y][x]
-    #sort distances
+
+    #pull distance from events in array
     distances = list(events.keys())
     distances.sort()
-
-    #Return events
     eventReturn = []
 
     #Getting the 5 closest events
     for counter in range(5):
         eventReturn.append(events[distances[counter]])
-
-    #Returing the list of 5 closest events
     return eventReturn
 
 #Get user input function.
 def getUserInput():
     #Loop till correct input given
-    # inputCoordinate = input("Please Input Coordinates:")
     while True:
         inputCoordinate = input("Please Input Coordinates:")
 
-        # Check if two values were entered
+        # Checks
         if (len(inputCoordinate.split(",")) != 2):
             print("ERROR: Enter a valid co-ordinate. Example: 4,2")
             continue
